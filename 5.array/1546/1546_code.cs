c1c6b04119980e5bdc315914ac6d886a9a055ba5
HellGateCheonan <55047686+HellGateCheonan@users.noreply.github.com>
2019-09-17 21:22:35 +0900

    Add files via upload

---
+++ 5.array/1546/1546_code.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ch5_6_1546
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            double max = 0;
+
+            string str = Console.ReadLine();
+            string[] str_unit = str.Split(' ');
+
+            double[] unit = new double[num];
+
+            for (int i = 0; i < num; i++)
+            {
+                unit[i] = Convert.ToDouble(str_unit[i]);
+            }
+
+            for (int i = 0; i < num; i++)
+            {
+                if (max < unit[i])
+                {
+                    max = unit[i];
+                }
+            }
+
+            double sum = 0;
+
+            for(int i = 0; i < num; i++)
+            {
+                double db = unit[i] / max * 100;
+                sum += db;
+            }
+
+            Console.WriteLine(sum / num);
+
+
+        }
+    }
+}
