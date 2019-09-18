7c7c5923d2d3babcf93c77f2ddf32a54950394e5
HellGateCheonan <55047686+HellGateCheonan@users.noreply.github.com>
2019-09-18 14:15:05 +0900

    Add files via upload

---
+++ 6.function/10872/10872_code.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ch6_4_10872_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int result = 1;
+            while (true)
+            {
+                if (num == 0)
+                    break;
+
+                result *= num;
+                if (num == 1)
+                    break;
+                else if (num > 1)
+                    num--;
+                
+                    
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
