0073bd6b736c8551ca5672bb2fa7d42b69528d2e
HellGateCheonan <55047686+HellGateCheonan@users.noreply.github.com>
2019-09-19 11:35:44 +0900

    Add files via upload

---
+++ 6.function/15596/15596_code.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ch6_1_15596
+{
+    class Program
+    {
+        int SumOfArray(int[] arr,int num)
+        {
+            int sum = 0;
+            for(int i = 0; i < num; i++)
+            {
+                sum += arr[i];
+            }
+
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
