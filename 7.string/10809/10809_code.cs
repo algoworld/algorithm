9b3efea655bf751615a6fc26ce712532efe47bf5
HellGateCheonan <55047686+HellGateCheonan@users.noreply.github.com>
2019-09-23 15:25:14 +0900

    Add files via upload

---
+++ 7.string/10809/10809_code.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ch7_3_10809
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            char[] ch = str.ToCharArray();
+            int num = 'z' - 'a';
+            int[] count = new int[num];
+            int[] location = new int[num];
+            for(int i = 0; i < ch.Length; i++)
+            {
+                int idx = ch[i] - 'a';
+                count[idx]++;
+                location[idx] = i;
+            }
+            string st = "";
+            for (int i = 0; i < num; i++)
+            {
+                if (count[i] > 0)
+                    st += Convert.ToString(location[i]);
+                else
+                    st += "-1";
+               
+                st += " ";
+            }
+
+            Console.WriteLine(st);
+
+        }
+    }
+}
