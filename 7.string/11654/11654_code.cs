34ed42c970294e77792d23e339c43b8f155a675c
HellGateCheonan <55047686+HellGateCheonan@users.noreply.github.com>
2019-09-23 11:51:55 +0900

    Add files via upload

---
+++ 7.string/11654/11654_code.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ch7_1__11654
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char asc = Convert.ToChar(Console.ReadLine());
+
+            int asc_num = Convert.ToInt32(asc);
+
+            Console.WriteLine(asc_num);
+        }
+    }
+}
