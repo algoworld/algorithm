e6ea948f6f8737c85dbcf5294472f6bee53521c4
HellGateCheonan <55047686+HellGateCheonan@users.noreply.github.com>
2019-09-23 15:05:40 +0900

    Add files via upload

---
+++ 7.string/11720/11720_code.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ch7_2_11720
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            string str = Console.ReadLine();
+            char[] ch_num = new char[num];
+            ch_num = str.ToCharArray();
+
+            int[] str_num = new int[num];
+            int sum = 0; 
+            for(int i = 0; i< num; i++)
+            {
+                sum += (ch_num[i]-'0');
+   
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
