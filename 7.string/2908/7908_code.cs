34c1a43d47629125a4be89320d3f74c89b546a55
HellGateCheonan <55047686+HellGateCheonan@users.noreply.github.com>
2019-09-24 12:22:27 +0900

    Add files via upload

---
+++ 7.string/2908/7908_code.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ch7_7_2908
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string[] str2 = str.Split(' ');
+            int[] num = new int[str2.Length];
+
+            for (int i = 0; i < str2.Length; i++)
+            {
+                char[] ch = str2[i].ToCharArray();
+                
+                Array.Reverse(ch);
+                
+                string temp = new string(ch);
+                num[i] = Convert.ToInt32(temp);
+                
+            }
+            int max = 0;
+            for(int i =0; i < num.Length; i++)
+            {
+                if (max < num[i])
+                    max = num[i];
+
+            }
+
+            Console.WriteLine(max);
+        }
+    }
+}
