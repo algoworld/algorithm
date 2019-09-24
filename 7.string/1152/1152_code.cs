68d47fe6db86e8d958d5584c1a51013f742b8db6
HellGateCheonan <55047686+HellGateCheonan@users.noreply.github.com>
2019-09-24 09:31:42 +0900

    Add files via upload

---
+++ 7.string/1152/1152_code.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ch7_6_1152
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string str_a = str.Trim();
+            string[] str_arr = str_a.Split(" ");
+            if (str_a == "")
+                Console.WriteLine(0);
+            else
+                Console.WriteLine(str_arr.Length);
+        }
+    }
+}
