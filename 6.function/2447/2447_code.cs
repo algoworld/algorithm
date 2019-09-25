0132eb1b64bca3f023b7cbfa92ab5d33123f380d
HellGateCheonan <55047686+HellGateCheonan@users.noreply.github.com>
2019-09-25 21:40:00 +0900

    Add files via upload

---
+++ 6.function/2447/2447_code.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ch6_5_2447
+{
+    class Program
+    {
+    
+
+        static void star(int modN, int i, int j)
+        {
+            if ((i / modN) % 3 == 1 && (j / modN) % 3 == 1)
+                Console.Write(" ");
+            else
+            {
+                if (modN / 3 == 0)
+                    Console.Write("*");
+                else
+                    star(modN / 3, i, j);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    Program.star(n, i, j);
+                Console.Write("\n");
+            }
+        }
+    }
+}
