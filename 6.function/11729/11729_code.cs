4ed8dc2789fdbfbe0732dca168db6b49f43c1706
HellGateCheonan <55047686+HellGateCheonan@users.noreply.github.com>
2019-09-28 09:47:30 +0900

    Add files via upload

---
+++ 6.function/11729/11729_code.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ch6_6_11729
+{
+    class Program
+    {
+
+        static int cnt = 1;
+
+        static void hanoi(int re, int f, int s, int none)
+        {
+
+            Program c;
+
+            if (re == 1) {
+                Console.WriteLine(f + " " + s);
+            }
+            else
+            {1172
+                Program.hanoi(re-1, f, none, s);
+                Program.hanoi(1, f, s, none);
+                Program.hanoi(re-1, none, s, f);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+                Program.cnt *= 2;
+
+            Console.Write(cnt - 1+ "\n");
+            Program.hanoi(n, 1, 3, 2);
+        }
+    }
+}
